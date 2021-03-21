2cde3f8f462385483fd77339fd67a2eb3a949209
belmiro-kunga <belmirokunga@gmail.com>
2021-03-21 12:50:43 +0100

    introdução

---
+++ sintaxe.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+ { 
+      cout << "Ola Alunos"; 
+return 0;
+}
+/*Objeto −  têm estados e comportamentos. Exemplo: Um cão
+ tem estados - cor, nome, raça, bem como comportamentos 
+- abanando, ladrar, comendo. Um objeto é uma instância de uma classe.
+
+Classe − Uma classe pode ser definida como um modelo ou projeto que descreve 
+os comportamentos eestados.
+
+Métodos − Um método é basicamente um comportamento. Uma classe pode conter muitos 
+métodos. É em métodos onde as lógicas são escritas, os dados são manipulados e
+todas as ações são executadas.
+
+Variáveis de instância − Cada objeto tem seu conjunto único de variáveis de 
+instância. O estado de um objeto é criado pelos valores atribuídos a essas 
+variáveis de instância.
+*/
